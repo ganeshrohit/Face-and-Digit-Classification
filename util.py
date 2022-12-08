fa21d95c46976eaa354a4d6bfde34b24f2598b56
Sehej Jain <sehejjain@outlook.com>
2022-12-07 21:38:30 -0500

    reorganized

--- util.py
+++ util.py
@@ -525,3 +525,35 @@ class ImagePrinter:
             print("new features:", pix)
             continue
       print(image) 
+      
+def basicFeatureExtractorDigit(datum):
+  """
+  Returns a set of pixel features indicating whether
+  each pixel in the provided datum is white (0) or gray/black (1)
+  """
+  a = datum.getPixels()
+
+  features = util.Counter()
+  for x in range(DIGIT_DATUM_WIDTH):
+    for y in range(DIGIT_DATUM_HEIGHT):
+      if datum.getPixel(x, y) > 0:
+        features[(x,y)] = 1
+      else:
+        features[(x,y)] = 0
+  return features
+
+def basicFeatureExtractorFace(datum):
+  """
+  Returns a set of pixel features indicating whether
+  each pixel in the provided datum is an edge (1) or no edge (0)
+  """
+  a = datum.getPixels()
+
+  features = util.Counter()
+  for x in range(FACE_DATUM_WIDTH):
+    for y in range(FACE_DATUM_HEIGHT):
+      if datum.getPixel(x, y) > 0:
+        features[(x,y)] = 1
+      else:
+        features[(x,y)] = 0
+  return features
